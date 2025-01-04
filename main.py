1a18236cd2ddec6f6a77e79f05fa2178b9451506
Denis Mwangi <deniskimani2022@gmail.com>
2025-01-04 11:07:16 +0300

    installed the packages

---
+++ main.py
@@ -0,0 +1,6 @@
+# filepath: /c:/Users/hp/OneDrive/Documents/crypto_binance_trading_bot/crypto_trading_bot/main.py
+def main():
+    print("Hello, Crypto Trading Bot!")
+
+if __name__ == "__main__":
+    main()
